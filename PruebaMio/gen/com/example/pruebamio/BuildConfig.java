31a7459ee7bfc744c59dc2c0a120ddcba96730e2
alejandro <alejandro@alejandro-B85M-D3H>
2014-03-18 17:29:03 +0100

    Hola caracola 1

---
+++ PruebaMio/gen/com/example/pruebamio/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.pruebamio;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ PruebaMio/src/com/example/pruebamio/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.pruebamio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Toast.makeText(getApplicationContext(), "Hola", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
